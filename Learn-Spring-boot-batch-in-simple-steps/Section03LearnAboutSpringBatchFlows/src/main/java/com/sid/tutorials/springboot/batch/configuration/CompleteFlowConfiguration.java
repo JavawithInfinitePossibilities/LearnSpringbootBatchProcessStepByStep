823f983ef32ea869cb68f8c4915008e4e7738a6f
kunmu <kunmu@Siddhanth-Sahu>
2023-06-14 01:28:47 +0530

    Added the counter in the program

--- Learn-Spring-boot-batch-in-simple-steps/Section03LearnAboutSpringBatchFlows/src/main/java/com/sid/tutorials/springboot/batch/configuration/CompleteFlowConfiguration.java
+++ Learn-Spring-boot-batch-in-simple-steps/Section03LearnAboutSpringBatchFlows/src/main/java/com/sid/tutorials/springboot/batch/configuration/CompleteFlowConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +31,7 @@ public class CompleteFlowConfiguration {
 
 	@Bean
 	public Job executeCompleteFlowJob(@Qualifier("firstFlow") Flow firstFlow, @Qualifier("lastFlow") Flow lastFlow) {
-		return jobBuilderFactory.get("SixthCompleteSpringBatchProject")
+		return jobBuilderFactory.get("SixthCompleteSpringBatchProject").incrementer(new RunIdIncrementer())
 				.start(firstFlow)
 				.next(step4).on("COMPLETED").to(lastFlow)
 				.end()
--- Learn-Spring-boot-batch-in-simple-steps/Section03LearnAboutSpringBatchFlows/src/main/java/com/sid/tutorials/springboot/batch/configuration/ExecuteParallelJobConfiguration.java
+++ Learn-Spring-boot-batch-in-simple-steps/Section03LearnAboutSpringBatchFlows/src/main/java/com/sid/tutorials/springboot/batch/configuration/ExecuteParallelJobConfiguration.java
@@ -6,10 +6,10 @@ package com.sid.tutorials.springboot.batch.configuration;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 /**
@@ -24,7 +24,7 @@ public class ExecuteParallelJobConfiguration {
 
 	@Bean
 	public Job executeParallelFlowJob(@Qualifier("firstFlow") Flow firstFlow, @Qualifier("lastFlow") Flow lastFlow) {
-		return jobBuilderFactory.get("SeventhParallelJobSpringBatchProject")
+		return jobBuilderFactory.get("SeventhParallelJobSpringBatchProject").incrementer(new RunIdIncrementer())
 				.start(firstFlow)
 				.split(new SimpleAsyncTaskExecutor())
 				.add(lastFlow)
